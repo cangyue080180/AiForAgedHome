c5f5a353cee458fadf6dde48d7db71d86bc628eb
cangyue <1053387324@qq.com>
2020-10-30 13:54:44 +0800

    添加全局异常捕获，防止无信息崩溃。

--- ClientForFront/AIForAgedClient/App.xaml.cs
+++ ClientForFront/AIForAgedClient/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows;
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
 
 namespace AIForAgedClient
 {
@@ -7,5 +10,41 @@ namespace AIForAgedClient
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            RegisterEvents();
+        }
+
+        private void RegisterEvents()
+        {
+            this.DispatcherUnhandledException += App_DispatcherUnhandledException;
+            TaskScheduler.UnobservedTaskException += new EventHandler<
+            UnobservedTaskExceptionEventArgs>(TaskScheduler_UnobservedTaskException);
+
+            AppDomain.CurrentDomain.UnhandledException += CurrentDomain_UnhandledException;
+        }
+
+        private void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e)
+        {
+
+        }
+
+        private void TaskScheduler_UnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            foreach (Exception item in e.Exception.InnerExceptions)
+            {
+                Console.WriteLine("异常类型：{0}{1}来自：{2}{3}异常内容：{4}",
+                item.GetType(), Environment.NewLine, item.Source,
+                Environment.NewLine, item.Message);
+            }
+            //将异常标识为已经观察到 
+            e.SetObserved();
+        }
+
+        private void App_DispatcherUnhandledException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+
+        }
     }
 }
