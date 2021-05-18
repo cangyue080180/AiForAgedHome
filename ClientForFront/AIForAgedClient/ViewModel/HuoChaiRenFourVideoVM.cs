15161e84e11c7cc362975566d8092946f2e6884f
cangyue <1053387324@qq.com>
2021-05-18 09:13:11 +0800

    添加关闭连接命令

--- ClientForFront/AIForAgedClient/ViewModel/HuoChaiRenFourVideoVM.cs
+++ ClientForFront/AIForAgedClient/ViewModel/HuoChaiRenFourVideoVM.cs
@@ -19,13 +19,13 @@ namespace AIForAgedClient.ViewModel
     {
         public UInt32 RoomId { get; set; }
 
-        TcpClient tcpClient;
-        NetworkStream stream;
-        CancellationTokenSource cancellationTokenSource;
-        readonly int remote_port = 8008;
-        string remote_ip = "127.0.0.1";
+        private TcpClient tcpClient;
+        private NetworkStream stream;
+        private CancellationTokenSource cancellationTokenSource;
+        private readonly int remote_port = 8008;
+        private string remote_ip = "127.0.0.1";
 
-        private async Task<byte[]> tcp_recv(NetworkStream stream,int data_len)
+        private async Task<byte[]> tcp_recv(NetworkStream stream, int data_len)
         {
             byte[] tempBuffer = new byte[data_len];
             try
@@ -37,7 +37,8 @@ namespace AIForAgedClient.ViewModel
                     int temp_recv_len = await stream.ReadAsync(tempBuffer, recv_len, data_len - recv_len);
                     recv_len += temp_recv_len;
                 }
-            }catch(Exception)
+            }
+            catch (Exception)
             {
                 Console.WriteLine("网络断开！");
                 //TODO: 提示用户网络存在问题，无法继续通信。
@@ -54,40 +55,39 @@ namespace AIForAgedClient.ViewModel
             Image4 = null;
 
             remote_ip = ConfigurationManager.AppSettings["TcpServerIp"];
-            tcpClient = new TcpClient(remote_ip,remote_port);
+            tcpClient = new TcpClient(remote_ip, remote_port);
             Console.WriteLine($"tcp connect: ({remote_ip}, {remote_port})");
 
             stream = tcpClient.GetStream();
 
             //发送角色数据包
-            Role role = new Role(3,1,1);
+            Role role = new Role(3, 1, 1);
             byte[] send_bytes = StructToBytes(role);
-            stream.WriteAsync(send_bytes,0,send_bytes.Length);
+            stream.WriteAsync(send_bytes, 0, send_bytes.Length);
             Console.WriteLine("send_role_packet");
 
             //发送获取图像请求命令包
-            VideoCmd videoCmd = new VideoCmd(1,5,RoomId,1);
+            VideoCmd videoCmd = new VideoCmd(1, 5, RoomId, 1);
             byte[] videoCmd_bytes = StructToBytes<VideoCmd>(videoCmd);
-            stream.WriteAsync(videoCmd_bytes,0,videoCmd_bytes.Length);
+            stream.WriteAsync(videoCmd_bytes, 0, videoCmd_bytes.Length);
             Console.WriteLine("send_get_video_packet");
 
             //接收图像数据并显示
             cancellationTokenSource = new CancellationTokenSource();
-            Task recvVideoTask = new Task(async()=> {
+            Task recvVideoTask = new Task(async () =>
+            {
                 try
                 {
                     await RecvVideo(cancellationTokenSource.Token);
                 }
                 catch (OperationCanceledException)
                 {
-
                 }
                 stream?.Close();
                 tcpClient?.Close();
-            },cancellationTokenSource.Token);
-           
+            }, cancellationTokenSource.Token);
+
             recvVideoTask.Start();
-            
         }
 
         private byte[] StructToBytes<T>(T videoCmd)
@@ -106,7 +106,7 @@ namespace AIForAgedClient.ViewModel
                 if (videoHeader.type == 2)
                 {
                     byte[] video_image_bytes = await tcp_recv(stream, (int)(videoHeader.len) - 4);
-                    Console.WriteLine("recv: "+video_image_bytes.Length);
+                    Console.WriteLine("recv: " + video_image_bytes.Length);
                     BitmapImage bitmap = null;
                     try
                     {
@@ -116,11 +116,11 @@ namespace AIForAgedClient.ViewModel
                         bitmap.EndInit();
                         bitmap.Freeze();
 
-                        if (Url1!=null && videoHeader.cameraId == uint.Parse(Url1))
+                        if (Url1 != null && videoHeader.cameraId == uint.Parse(Url1))
                         {
                             Image1 = bitmap;
                         }
-                        else if (Url2!=null && videoHeader.cameraId == uint.Parse(Url2))
+                        else if (Url2 != null && videoHeader.cameraId == uint.Parse(Url2))
                         {
                             Image2 = bitmap;
                         }
@@ -134,7 +134,6 @@ namespace AIForAgedClient.ViewModel
                         }
                         else
                         {
-
                         }
                     }
                     catch (System.NotSupportedException)
@@ -143,7 +142,6 @@ namespace AIForAgedClient.ViewModel
                     }
                 }
             }
-            
         }
 
         private T BytesToStruct<T>(byte[] video_header_bytes)
@@ -154,14 +152,16 @@ namespace AIForAgedClient.ViewModel
         public override void Stop()
         {
             //发送关闭图像命令
-            VideoCmd videoCmd = new VideoCmd(1, 5, RoomId, 0);
-            byte[] videoCmd_bytes = StructToBytesHelper.StructToBytes<VideoCmd>(videoCmd);
-            stream.Write(videoCmd_bytes, 0, videoCmd_bytes.Length);
-            
-            cancellationTokenSource.Cancel();
-            
-        }
+            //VideoCmd videoCmd = new VideoCmd(1, 5, RoomId, 0);
+            //byte[] videoCmd_bytes = StructToBytesHelper.StructToBytes<VideoCmd>(videoCmd);
+            //stream.Write(videoCmd_bytes, 0, videoCmd_bytes.Length);
+            //发送关闭连接命令
+            Role exit_role = new Role(4, 1, 1);
+            byte[] exit_bytes = StructToBytesHelper.StructToBytes<Role>(exit_role);
+            stream.Write(exit_bytes, 0, exit_bytes.Length);
 
+            cancellationTokenSource.Cancel();
+        }
     }
 
     [StructLayout(LayoutKind.Sequential, Pack = 1)]
@@ -171,7 +171,7 @@ namespace AIForAgedClient.ViewModel
         public UInt32 len;
         public byte role;
 
-        public Role(byte type,UInt32 len,byte role)
+        public Role(byte type, UInt32 len, byte role)
         {
             this.type = type;
             this.len = len;
@@ -187,7 +187,7 @@ namespace AIForAgedClient.ViewModel
         public UInt32 roomId;
         public byte isOpen;
 
-        public VideoCmd(byte type,UInt32 len,UInt32 roomId,byte isOpen)
+        public VideoCmd(byte type, UInt32 len, UInt32 roomId, byte isOpen)
         {
             this.type = type;
             this.len = len;
@@ -203,7 +203,7 @@ namespace AIForAgedClient.ViewModel
         public UInt32 len;
         public UInt32 cameraId;
 
-        public VideoHeader(byte type,UInt32 len, UInt32 cameraId)
+        public VideoHeader(byte type, UInt32 len, UInt32 cameraId)
         {
             this.type = type;
             this.len = len;
